f6d26e7caef4cacba6a503ff68e78cfb677eb15b
Бажен Левковець <bazhen.paseka@gmail.com>
2018-07-07 16:48:54 +0300

    struct added

--- lcd1602_fc113_sm.c
+++ lcd1602_fc113_sm.c
@@ -1,20 +1,21 @@
 #define ADR_I2C_FC113 0x27
+#include <string.h>
 #include "lcd1602_fc113_sm.h"
 
 //==========================================================================
-void LCD1602_Write_Instruction_4bit(uint8_t inputE0)
+void LCD1602_Write_Instruction_4bit(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t inputE0)
 {
 	uint8_t inputE1;
 	inputE1 = inputE0 + 4;
 
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputE1, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputE1, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputE0, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputE0, 1, 100);
 	HAL_Delay(1);
 }
 //==========================================================================
-void LCD1602_Write_Instruction_8bit(uint8_t input)
+void LCD1602_Write_Instruction_8bit(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
 {
 	uint8_t inputStE0;
 	uint8_t inputStE1;
@@ -27,18 +28,18 @@ void LCD1602_Write_Instruction_8bit(uint8_t input)
 	inputMlE1 = inputMlE0 + 4;
 
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputStE1, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputStE1, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputStE0, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputStE0, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputMlE1, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputMlE1, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputMlE0, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputMlE0, 1, 100);
 	HAL_Delay(1);
 }
 
 //==========================================================================
-void LCD1602_Write_Data(uint8_t input)
+void LCD1602_Write_Data(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
 	{
 	uint8_t inputStE0;
 	uint8_t inputStE1;
@@ -50,90 +51,92 @@ void LCD1602_Write_Data(uint8_t input)
 	inputMlE0 = (input<<4) + 9; //((input<<4) * 0xF0) + 1;
 	inputMlE1 = inputMlE0 + 4; // LED Light
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputStE1, 1, 100);
+	//HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputStE1, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputStE1, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputStE0, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputStE0, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputMlE1, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputMlE1, 1, 100);
 	HAL_Delay(1);
-	HAL_I2C_Master_Transmit(&hi2c1, ADR_I2C_FC113<<1, &inputMlE0, 1, 100);
+	HAL_I2C_Master_Transmit(lcd1602_fc113_handler->i2c, lcd1602_fc113_handler->device_i2c_address<<1, &inputMlE0, 1, 100);
 	HAL_Delay(1);
 	}
 
 //==========================================================================
-void LCD1602_Init(void)
+void LCD1602_Init(lcd1602_fc113_struct *lcd1602_fc113_handler )
 	{
 	HAL_Delay(15);
-	LCD1602_Write_Instruction_4bit(0x30);	// step 1: 0011 0000
+	LCD1602_Write_Instruction_4bit(lcd1602_fc113_handler, 0x30);	// step 1: 0011 0000
 	HAL_Delay(5);
-	LCD1602_Write_Instruction_4bit(0x30);	// step 2: 0011 0000
-	LCD1602_Write_Instruction_4bit(0x30);	// step 3: 0011 0000
-	LCD1602_Write_Instruction_4bit(0x20);	// step 4: 0010 0000
+	LCD1602_Write_Instruction_4bit(lcd1602_fc113_handler, 0x30);	// step 2: 0011 0000
+	LCD1602_Write_Instruction_4bit(lcd1602_fc113_handler, 0x30);	// step 3: 0011 0000
+	LCD1602_Write_Instruction_4bit(lcd1602_fc113_handler, 0x20);	// step 4: 0010 0000
 	// 4-bit is active
-	LCD1602_Write_Instruction_8bit(0x28);		// step 5: 0010 1000 NF
-	LCD1602_Write_Instruction_8bit(0x08);		// step 6: 0000 1000
-	LCD1602_Write_Instruction_8bit(0x01);		// step 7: 0000 0001
-	LCD1602_Write_Instruction_8bit(0x06);		// step 8: 0000 0110
-	LCD1602_Write_Instruction_8bit(0x0C);		// step 9: 0000 1100 CB
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x28);		// step 5: 0010 1000 NF
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x08);		// step 6: 0000 1000
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x01);		// step 7: 0000 0001
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x06);		// step 8: 0000 0110
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x0C);		// step 9: 0000 1100 CB
 	}
 //==========================================================================
-void LCD1602_Clear(void)
+void LCD1602_Clear(lcd1602_fc113_struct *lcd1602_fc113_handler)
 {
-	LCD1602_Write_Instruction_8bit(0x01); 	// 0x01 clear disp
-	LCD1602_Write_Instruction_8bit(0x03);		// 0x03 cursor to start
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x01); 	// 0x01 clear disp
+	LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x03);		// 0x03 cursor to start
 }
 //==========================================================================
-void LCD1602_Cursor_Shift_Right(uint32_t input)
+void LCD1602_Cursor_Shift_Right(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
 {
 	for ( ; input>0; input--)
 	{
-		LCD1602_Write_Instruction_8bit(0x14);		// Shift Cursor Right
-	}
-}
-//==========================================================================
-
-void LCD1602_Cursor_Shift_Left(uint32_t input)
-{
-	for ( ; input>0; input--)
-	{
-		LCD1602_Write_Instruction_8bit(0x10);		// Shift Cursor Right
+		LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x14);		// Shift Cursor Right
 	}
 }
 //==========================================================================
 
-void LCD1602_Cursor_Return(void)
-{
-		LCD1602_Write_Instruction_8bit(0x2);		// Cursor Return
-}
-//==========================================================================
-
-void LCD1602_Display_Shift_Right(uint32_t input)
+void LCD1602_Cursor_Shift_Left(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
 {
 	for ( ; input>0; input--)
 	{
-		LCD1602_Write_Instruction_8bit(0x1C);		// Shift Display Left
+		LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x10);		// Shift Cursor Right
 	}
 }
 //==========================================================================
 
-void LCD1602_Display_Shift_Left(uint32_t input)
+void LCD1602_Cursor_Return(lcd1602_fc113_struct *lcd1602_fc113_handler)
+{
+		LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x2);		// Cursor Return
+}
+//==========================================================================
+
+void LCD1602_Display_Shift_Right(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
 {
 	for ( ; input>0; input--)
 	{
-		LCD1602_Write_Instruction_8bit(0x18);		// Shift Display Right
+		LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x1C);		// Shift Display Left
+	}
+}
+//==========================================================================
+
+void LCD1602_Display_Shift_Left(lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input)
+{
+	for ( ; input>0; input--)
+	{
+		LCD1602_Write_Instruction_8bit(lcd1602_fc113_handler, 0x18);		// Shift Display Right
 	}
 }
 //==========================================================================
 
 //======================================================================
-void I2C_ScanBus(void)
+void I2C_ScanBus(lcd1602_fc113_struct *lcd1602_fc113_handler)
 {
 	uint8_t i = 0;
 	char DataChar[32];
 	uint32_t Device =0;
 
-	for (i = 0x07; i < 0x78; i++) {
-		if (HAL_I2C_IsDeviceReady(&hi2c1, i << 1, 10, 100) == HAL_OK)
+	for (i = 0x07; i < 0x78; i++)
+	{
+		if (HAL_I2C_IsDeviceReady(lcd1602_fc113_handler->i2c, i << 1, 10, 100) == HAL_OK)
 			{
 			Device++;
 			switch (i)
@@ -147,23 +150,23 @@ void I2C_ScanBus(void)
 				default:   sprintf(DataChar,"%d) Unknown", (int)Device ); break;
 			}// end switch
 
-			LCD1602_Print_Char((uint8_t *)DataChar, strlen(DataChar));
+			LCD1602_Print_Char(lcd1602_fc113_handler, (char *)DataChar, strlen(DataChar));
 
 			if (Device%2 == 0)
 				{
 				HAL_Delay(1000);
-				LCD1602_Clear();
+				LCD1602_Clear(lcd1602_fc113_handler);
 				}
 			else
 				{
 				HAL_Delay(400);
-				LCD1602_Cursor_Shift_Right(30);
+				LCD1602_Cursor_Shift_Right(lcd1602_fc113_handler, 30);
 				}
 		} //end if HAL I2C
 
 	}// end for i
 	sprintf(DataChar,"Scan I2C End.\n");
-	LCD1602_Print_Char((uint8_t *)DataChar, strlen(DataChar));
+	LCD1602_Print_Char(lcd1602_fc113_handler, (char *)DataChar, strlen(DataChar));
 
 	HAL_Delay(500);
 }// end void I2C_ScanBus
@@ -171,18 +174,18 @@ void I2C_ScanBus(void)
 
 //=================================================================
 
-void LCD1602_Print_Char(uint8_t *DataChar, int32_t SizeChar)
+void LCD1602_Print_Char(lcd1602_fc113_struct *lcd1602_fc113_handler, char *DataChar, uint8_t SizeChar)
 	{
-	for (int i=0; i<SizeChar; i++)
+	for (uint8_t i=0; i<SizeChar; i++)
 		{
 		if (DataChar[i] != '\n')
-			LCD1602_Write_Data(DataChar[i]);
+			LCD1602_Write_Data(lcd1602_fc113_handler, DataChar[i]);
 
 		if (DataChar[i] == '\n')
-			LCD1602_Cursor_Shift_Right(40-i);
+			LCD1602_Cursor_Shift_Right(lcd1602_fc113_handler, 40-i);
 
 		if (i == 15)
-			LCD1602_Cursor_Shift_Right(40-i-1);
+			LCD1602_Cursor_Shift_Right(lcd1602_fc113_handler, 40-i-1);
 
 		}
 	}
--- lcd1602_fc113_sm.h
+++ lcd1602_fc113_sm.h
@@ -1,16 +1,49 @@
+#ifndef LCD1602_FC113_SM_H_
+#define LCD1602_FC113_SM_H_
 
 #include "stm32f1xx_hal.h"
 #include "i2c.h"
 
-void LCD1602_Write_Instruction_4bit(uint8_t);
-void LCD1602_Write_Instruction_8bit(uint8_t);
-void LCD1602_Write_Data(uint8_t);
-void LCD1602_Init(void);
-void LCD1602_Clear(void);
-void LCD1602_Cursor_Shift_Right(uint32_t input);
-void LCD1602_Cursor_Shift_Left(uint32_t input);
-void LCD1602_Display_Shift_Left(uint32_t input);
-void LCD1602_Display_Shift_Right(uint32_t input);
-void LCD1602_Cursor_Return(void);
-void I2C_ScanBus(void);
-void LCD1602_Print_Char(uint8_t *DataChar, int32_t);
+typedef struct
+{
+	I2C_HandleTypeDef 	*i2c;
+	uint8_t 			 device_i2c_address;
+} lcd1602_fc113_struct;
+
+void LCD1602_Init                   (lcd1602_fc113_struct *lcd1602_fc113_handler );
+void LCD1602_Clear                  (lcd1602_fc113_struct *lcd1602_fc113_handler );
+void LCD1602_Cursor_Return          (lcd1602_fc113_struct *lcd1602_fc113_handler );
+void I2C_ScanBus                    (lcd1602_fc113_struct *lcd1602_fc113_handler );
+
+void LCD1602_Write_Instruction_4bit (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+void LCD1602_Write_Instruction_8bit (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+void LCD1602_Write_Data             (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+
+void LCD1602_Cursor_Shift_Right     (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+void LCD1602_Cursor_Shift_Left      (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+void LCD1602_Display_Shift_Left     (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+void LCD1602_Display_Shift_Right    (lcd1602_fc113_struct *lcd1602_fc113_handler, uint8_t input);
+
+void LCD1602_Print_Char             (lcd1602_fc113_struct *lcd1602_fc113_handler, char *DataChar, uint8_t);
+
+	/*
+	#define ADR_I2C_FC113 0x27
+	uint8_t SizeChar;
+	char DataChar[32];
+
+	lcd1602_fc113_struct h1_lcd1602_fc113 =
+	{
+		.i2c = &hi2c1,
+		.device_i2c_address = ADR_I2C_FC113
+	};
+
+	LCD1602_Init(&h1_lcd1602_fc113);
+	I2C_ScanBus(&h1_lcd1602_fc113);
+	sprintf(DataChar,"AB1,AB2,AB3,AB4,AB5,AB6,AB7.");
+	SizeChar = strlen(DataChar);
+	LCD1602_Print_Char(&h1_lcd1602_fc113, DataChar, SizeChar);
+	HAL_Delay(1000);
+	LCD1602_Clear(&h1_lcd1602_fc113);
+	*/
+
+#endif
