73839a2def88318c9972026c680f7f78bc95049c
Бажен Левковець <bazhen.paseka@gmail.com>
2020-04-06 12:33:25 +0300

    deleted techmacker submodule
    .

--- lcd1602_fc113_sm.c
+++ lcd1602_fc113_sm.c
@@ -132,12 +132,12 @@ void LCD1602_Display_Shift_Left(lcd1602_fc113_struct *lcd1602_fc113_handler, uin
 //==========================================================================
 
 //======================================================================
-void I2C_ScanBus_to_LCD1602(lcd1602_fc113_struct *lcd1602_fc113_handler)
+void LCD1602_scan_I2C_bus(lcd1602_fc113_struct *lcd1602_fc113_handler)
 {
 	char i2c_scan_buff[32];
 	int  i2c_scan_device = 0;
 
-	sprintf(i2c_scan_buff,"Start scan I2C\n");
+	sprintf(i2c_scan_buff,"I2C scan start\n");
 	LCD1602_Print_Line(lcd1602_fc113_handler, i2c_scan_buff, strlen(i2c_scan_buff));
 
 	HAL_Delay(500);
@@ -151,7 +151,8 @@ void I2C_ScanBus_to_LCD1602(lcd1602_fc113_struct *lcd1602_fc113_handler)
 			{
 				case 0x23: sprintf(i2c_scan_buff,"%d) BH-1750", i2c_scan_device ); break;
 				case 0x27: sprintf(i2c_scan_buff,"%d) FC-113 ", i2c_scan_device ); break;
-				case 0x57: sprintf(i2c_scan_buff,"%d) AT24C32", i2c_scan_device ); break;
+				//case 0x57: sprintf(i2c_scan_buff,"%d) AT24C32", i2c_scan_device ); break;
+				case 0x57: sprintf(i2c_scan_buff,"%d) MAX30100", i2c_scan_device ); break;
 				case 0x68: sprintf(i2c_scan_buff,"%d) DS-3231", i2c_scan_device ); break;
 				case 0x76: sprintf(i2c_scan_buff,"%d) BMP-280", i2c_scan_device ); break;
 				case 0x77: sprintf(i2c_scan_buff,"%d) BMP-180", i2c_scan_device ); break;
@@ -173,11 +174,11 @@ void I2C_ScanBus_to_LCD1602(lcd1602_fc113_struct *lcd1602_fc113_handler)
 		} //end if HAL I2C
 
 	}// end for i
-	sprintf(i2c_scan_buff,"Scan I2C End\r\n");
+	sprintf(i2c_scan_buff,"I2C  scan  end\r\n");
 	LCD1602_Print_Line(lcd1602_fc113_handler, i2c_scan_buff, strlen(i2c_scan_buff));
 
 	HAL_Delay(500);
-}// end I2C_ScanBus_to_LCD1602
+}// end LCD1602_scan_I2C_bus
 //======================================================================
 
 //=================================================================
@@ -208,3 +209,35 @@ void LCD1602_Print_Line(lcd1602_fc113_struct *lcd1602_fc113_handler, char* DataC
 }
 //=================================================================
 
+void LCD1602_Scan_I2C_to_UART(lcd1602_fc113_struct * _lcd1602_fc113_handler, UART_HandleTypeDef * _huart) {
+	char DataChar[32];
+	int device_serial_numb = 0;
+
+	sprintf(DataChar,"\tStart scan I2C\r\n");
+	HAL_UART_Transmit(_huart, (uint8_t *)DataChar, strlen(DataChar), 100);
+	HAL_Delay(100);
+
+	for ( int device_i2c_address_int = 0x07; device_i2c_address_int < 0x78; device_i2c_address_int++) {
+		if (HAL_I2C_IsDeviceReady( _lcd1602_fc113_handler->i2c , device_i2c_address_int << 1, 10, 100) == HAL_OK) {
+			device_serial_numb++;
+			switch (device_i2c_address_int) {
+				case 0x23: sprintf(DataChar,"%d) BH1750", device_serial_numb ); break;
+				case 0x27: sprintf(DataChar,"%d) FC113 ", device_serial_numb ); break;
+				case 0x38: sprintf(DataChar,"%d) PCF8574", device_serial_numb ); break;
+				//case 0x57: sprintf(DataChar,"%d) AT24C32", device_serial_numb ); break;
+				case 0x57: sprintf(DataChar,"%d) MAX30100", device_serial_numb ); break;
+				case 0x68: sprintf(DataChar,"%d) DS3231", device_serial_numb ); break;
+				//case 0x68: sprintf(DataChar_I2C,"%d) MPU9250", device_serial_numb ); break;
+				case 0x76: sprintf(DataChar,"%d) BMP280", device_serial_numb ); break;
+				case 0x77: sprintf(DataChar,"%d) BMP180", device_serial_numb ); break;
+				default:   sprintf(DataChar,"%d) Unknown", device_serial_numb ); break;
+			}// end switch
+			sprintf(DataChar,"%s Adr: %x\r\n", DataChar, device_i2c_address_int);
+			HAL_UART_Transmit(_huart, (uint8_t *)DataChar, strlen(DataChar), 100);
+			HAL_Delay(10);
+		} //end if HAL I2C1
+	} // end for device_i2c_address_int i2c1
+	sprintf(DataChar,"\tEnd scan I2C\r\n");
+	HAL_UART_Transmit(_huart, (uint8_t *)DataChar, strlen(DataChar), 100);
+	//HAL_Delay(100);
+}// end void I2C_ScanBus
--- lcd1602_fc113_sm.h
+++ lcd1602_fc113_sm.h
@@ -8,6 +8,7 @@
 	#include <string.h>
 	#include "stdio.h"
 
+	#include "lcd1602_local_config.h"
 
 //*******************************************************************
 
@@ -22,7 +23,8 @@
 void LCD1602_Print_Line             (lcd1602_fc113_struct *lcd1602_fc113_handler, char *DataChar, uint8_t);
 
 void LCD1602_Init                   (lcd1602_fc113_struct *lcd1602_fc113_handler );
-void I2C_ScanBus_to_LCD1602                    (lcd1602_fc113_struct *lcd1602_fc113_handler );
+void LCD1602_scan_I2C_bus			(lcd1602_fc113_struct *lcd1602_fc113_handler );
+void LCD1602_Scan_I2C_to_UART		(lcd1602_fc113_struct *lcd1602_fc113_handler, UART_HandleTypeDef * _huart);
 void LCD1602_Clear                  (lcd1602_fc113_struct *lcd1602_fc113_handler );
 void LCD1602_Cursor_Return          (lcd1602_fc113_struct *lcd1602_fc113_handler );
 
@@ -45,7 +47,7 @@ void LCD1602_Cursor_Shift_Right     (lcd1602_fc113_struct *lcd1602_fc113_handler
 		};
 
 		LCD1602_Init(&h1_lcd1602_fc113);
-		I2C_ScanBus_to_LCD1602(&h1_lcd1602_fc113);
+		LCD1602_scan_I2C_bus(&h1_lcd1602_fc113);
 
 		LCD1602_Clear(&h1_lcd1602_fc113);
 		sprintf(uart_buffer_c,"Steps Counter\r\n");
